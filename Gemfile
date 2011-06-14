5aece0dc3e32bf96eedf85809c1e0e798dacb193
Allan Moster <mosterbills@gmail.com>
2011-06-14 09:42:02 -0400

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'http://rubygems.org'
+
+# Generic gem dependencies first
+gem 'rails', '3.0.7'
+gem 'aws-s3', :require => 'aws/s3'
+gem 'dynamic_form'
+gem 'heroku'
+gem 'memcache-client'
+
+# Followed by spree itself first, all spree-specific extensions second
+gem 'spree', '0.60.0'
+gem 'rake', '0.8.7'
+
+# Dev/Test gems
+group :development, :test do
+gem 'sqlite3'
+gem 'webrat'
+gem 'cucumber-rails'
+gem 'rspec-rails'
+end
+
+# EOF
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pbdstore::Application.config.secret_token = 'd34efd08058752dfa1a2ae33636f73fee8e58c120fe4869d756f9910acbf70ecc0bc8161e8cbb7fb449f0d4d63df4dc8eb9ad5f745a81f3d8fc006f330e78b3f'
